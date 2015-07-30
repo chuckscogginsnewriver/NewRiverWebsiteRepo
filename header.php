ea111f9113be03ada9122175b6fc8569d986b97e
Chuck Scoggins <chuck@newriver.church>
2015-07-30 19:35:26 -0400

    email form updates.

--- header.php
+++ header.php
@@ -83,7 +83,7 @@
 		});
 		</script-->
 		
-		<!--script>
+		<script>
 jQuery(document).ready(function () {
     //if cookie hasn't been set...
     if (document.cookie.indexOf("ModalShown=true")<0) {
@@ -95,7 +95,7 @@ jQuery(document).ready(function () {
         document.cookie = "ModalShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
     }
 });
-</script-->
+</script>
 
 	</head>
 
